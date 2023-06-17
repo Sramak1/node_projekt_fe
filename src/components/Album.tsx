10b294d4699ff45a5ac03e61c44ace98c026123d
Sramak1 <mark.centrih2@scv.si>
2023-06-17 10:22:43 +0100

    login and register

---
+++ src/components/Album.tsx
@@ -0,0 +1,18 @@
+import Task from "./Task.tsx";
+
+const Album = () => {
+  return(
+      <>
+          <div className="album py-5 bg-body-tertiary">
+              <div className="container">
+
+                  <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                      <Task/>
+                      <Task/>
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+}
+export default Album;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,89 @@
+import {SyntheticEvent, useState} from "react";
+import {Navigate} from "react-router-dom";
+import './Register.css';
+import axios from 'axios';
+
+
+
+const Register = () => {
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password1,setPassword1] = useState('');
+    const [password2,setPassword2] = useState('');
+
+    const [errorText, setErrorText] = useState('');
+    const [redirect,setRedirect]=useState(false);
+
+    const submit = async (e:SyntheticEvent) =>{
+        e.preventDefault();
+
+        if(password1!=password2){
+            setErrorText('Gesli se ne ujemata');
+        }
+        if(password1 == password2) {
+            const data = {
+                "first_name": firstName,
+                "last_name": lastName,
+                email,
+                "password": password1
+            };
+            const res = await axios.post('http://localhost:3000/user', data);
+
+            if (res.status != 201)
+            {
+                setErrorText('Napaka v registracijskih podatkih');
+                console.log(res.data);
+            }
+            if(res.status == 201)
+            {
+                setRedirect(true);
+            }
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/login'}/>;
+    }
+  return(
+      <>
+          <main className="form-signin w-100 m-auto">
+              <h2 className = "error">{errorText}</h2>
+              <form onSubmit={submit}>
+                  <h1 className="h3 mb-3 fw-normal">Please register</h1>
+                  <div className="form-floating">
+                      <input type="text" className="form-control" id="floatingInput"
+                             placeholder="Input your first name"
+                             onChange={(e)=>setFirstName(e.target.value)}/>
+                      <label htmlFor="floatingInput">First name</label>
+                  </div>
+                  <div className="form-floating">
+                      <input type="text" className="form-control" id="floatingInput"
+                             placeholder="Imput your last name"
+                             onChange={(e)=>setLastName(e.target.value)}/>
+                      <label htmlFor="floatingInput">Last name</label>
+                  </div>
+                  <div className="form-floating">
+                      <input type="email" className="form-control" id="floatingInput"
+                             placeholder="name@example.com"
+                             onChange={(e)=>setEmail(e.target.value)}/>
+                      <label htmlFor="floatingInput">Email address</label>
+                  </div>
+                  <div className="form-floating">
+                      <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                             onChange={(e)=>setPassword1(e.target.value)}/>
+                      <label htmlFor="floatingPassword">Password</label>
+                  </div>
+                  <div className="form-floating">
+                      <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                             onChange={(e)=>setPassword2(e.target.value)}/>
+                      <label htmlFor="floatingPassword">Repeat password</label>
+                  </div>
+
+                  <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+              </form>
+          </main>
+      </>
+  )
+}
+export default Register;
